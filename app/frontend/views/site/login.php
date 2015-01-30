270a525e9ad7e98ecf7bd6e93264f352dca9db73
Julia Makhnach <julia@meta.ua>
2015-01-30 18:38:22 +0200

    commit for login

--- app/frontend/views/site/login.php
+++ app/frontend/views/site/login.php
@@ -9,24 +9,48 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+<body class="html not-front not-logged-in no-sidebars page-user page-user-register section-user login-pages" >
+    <div class="ie-browser-header">
+    You are using Internet Explorer 8. Some features might not work or display properly. It is highly recommended to update the browser or use a different one.        </div>
+    <p id="skip-link">
+    <a href="#main-menu" class="element-invisible element-focusable">Jump to navigation</a>
+    </p>
+    <div class="header high-header">
+        <a href="/" class="logo">
+            <img src="../images/logo_0.png" alt="Ebanq"/>
+        </a>    </div>
+        <div class="content">
+            <ul class="big-screen-tabs">
+                <li><a href="login.htm">Log In</a></li>
+                <li class="or">or</li>
+                <li class="active"><a href="login-sign-up.htm" class="active">Sign Up</a></li>
+            </ul>
 
-    <div class="row">
-        <div class="col-lg-5">
+
+
+
+
+<div class="login-form">
+
+
+ <!--   <h1><?= Html::encode($this->title) ?></h1>-->
+
+  <!-- <p>Please fill out the following fields to login:</p> -->
+
+    <!--<div class="row">-->
+       <!-- <div class="col-lg-5">-->
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+                <?= $form->field($model, 'username',['inputOptions' => ['id'=>"edit-name", 'placeholder'=>"Name"]]) ?>
+                <?= $form->field($model, 'password',['inputOptions' => ['id'=>"edit-pass", 'placeholder'=>"Password"]])->passwordInput() ?>
+                <!--  <?= $form->field($model, 'rememberMe')->checkbox() ?> -->
+                <div>
+                     <?= Html::a('Forgot your password?', ['site/request-password-reset']) ?>
                 </div>
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('SING IN', ['class' => 'btn btn-success form-submit', 'name' => 'op']) ?>
                 </div>
             <?php ActiveForm::end(); ?>
-        </div>
-    </div>
+        <!--</div>-->
+   <!-- </div>-->
 </div>
